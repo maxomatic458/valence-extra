82fd6b4a5cbac7dc6e7174ffa047654861006143
maxomatic458 <104733404+maxomatic458@users.noreply.github.com>
2025-02-01 14:36:47 +0100

    build stuff

--- crates/building/src/lib.rs
+++ crates/building/src/lib.rs
@@ -1,4 +1,20 @@
+use std::time::Instant;
+
 use valence::prelude::*;
+
+/// Attached to every player that is able to build.
+#[derive(Component)]
+pub struct BuildState {
+    /// Last time the player placed a block.
+    pub last_place: Instant,
+    /// The build config for the player.
+    pub build_config: PlayerBuildConfig,
+}
+
+/// Configuration 
+pub struct PlayerBuildConfig {
+    
+}
 
 pub struct BuildPlugin;
 
