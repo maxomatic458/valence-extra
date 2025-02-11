5ae5aa680a4bc3016d815acae8df12981d76dc90
maxomatic458 <104733404+maxomatic458@users.noreply.github.com>
2025-02-11 20:08:39 +0100

    more work on building

--- crates/building/src/lib.rs
+++ crates/building/src/lib.rs
@@ -1,6 +1,5 @@
-use std::time::Instant;
-
-use valence::prelude::*;
+use std::time::{Duration, Instant};
+use valence::{math::Aabb, prelude::*};
 
 /// Attached to every player that is able to build.
 #[derive(Component)]
@@ -11,9 +10,28 @@ pub struct BuildState {
     pub build_config: PlayerBuildConfig,
 }
 
-/// Configuration 
+/// Configuration
 pub struct PlayerBuildConfig {
-    
+    /// A Cooldown for placing blocks.
+    pub place_cooldown: Duration,
+    /// A callback when the player tries to place a block.
+    /// If this callback returns `true`, the block will be placed.
+    ///
+    /// Note: This callback will be called after all other checks e.g. the build cooldown.
+    ///
+    /// The parameters are: `block_kind`, `position_of_placed_block`, `player_inventory`.
+    pub on_try_place: fn(BlockKind, DVec3, &mut Inventory) -> bool,
+    /// If placement checks for entity hitboxes should be ignored.
+    pub ignore_entity_hitboxes_for_placement: bool,
+}
+
+/// Component that is attached to entities that prevent block placement (based on their hitbox).
+#[derive(Component, Default)]
+pub struct PreventBlockPlacement {
+    /// The hitbox to be used for block placement checks.
+    ///
+    /// If `None` the entity's hitbox will be used.
+    pub hitbox: Option<Aabb>,
 }
 
 pub struct BuildPlugin;
